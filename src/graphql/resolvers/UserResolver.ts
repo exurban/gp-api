97fe95329811156a6e18077897f8bcbd0589eb2e
Brad Gibbs <bradgibbs@mac.com>
2021-04-22 15:29:59 -0500

    new db credentials

--- src/graphql/resolvers/UserResolver.ts
+++ src/graphql/resolvers/UserResolver.ts
@@ -1,10 +1,34 @@
-import { Arg, Authorized, Int, Query, Resolver } from 'type-graphql';
+import {
+  Arg,
+  Authorized,
+  Ctx,
+  Field,
+  InputType,
+  Int,
+  Mutation,
+  Query,
+  Resolver,
+} from 'type-graphql';
 import { InjectRepository } from 'typeorm-typedi-extensions';
 import { Repository } from 'typeorm';
+import jwt from 'jsonwebtoken';
 import * as dotenv from 'dotenv';
 import User from '../entities/User';
 
 dotenv.config();
+
+type Context = {
+  user: User;
+};
+
+@InputType()
+class GetApiTokenInput {
+  @Field()
+  userId: number;
+
+  @Field()
+  email: string;
+}
 
 @Resolver(() => User)
 export default class UserResolver {
@@ -12,6 +36,31 @@ export default class UserResolver {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>
   ) {}
+
+  @Mutation(() => String)
+  async getApiToken(
+    @Arg('input', () => GetApiTokenInput) input: GetApiTokenInput
+  ): Promise<string> {
+    console.log(`received Get API Token request`);
+    // look up compoundId, get its user and check that the email uses the one sent
+    const user = await this.userRepository.findOne(input.userId);
+
+    // * verify email
+    if (user && user.email === input.email) {
+      const token = jwt.sign(
+        {
+          id: user.id,
+          email: user.email,
+        },
+        process.env.JWT_SECRET as string
+      );
+
+      console.log(`Sending token to user already in DB.`);
+      return token;
+    } else {
+      throw new Error(`Sign in credentials don't match.`);
+    }
+  }
 
   //* Queries
   // make this a query builder query
@@ -25,5 +74,12 @@ export default class UserResolver {
   @Query(() => User)
   async user(@Arg('id', () => Int) id: number): Promise<User | undefined> {
     return await this.userRepository.findOne(id);
+  }
+
+  @Authorized('USER')
+  @Query(() => Int)
+  async me(@Ctx() context: Context): Promise<number> {
+    const userId = context.user.id;
+    return userId;
   }
 }
